6c47331a215928378531309f41c4dbfbcf70a743
Vixu <lonevixu@gmail.com>
2024-07-14 14:01:56 +0200

    optimize tree

--- unlox-tree/src/lib.rs
+++ unlox-tree/src/lib.rs
@@ -8,7 +8,10 @@ pub struct Tree<T> {
 pub struct Node<T> {
     value: T,
     parent: Option<Index>,
-    children: Vec<Index>,
+    first_child: Option<Index>,
+    last_child: Option<Index>,
+    prev_sibling: Option<Index>,
+    next_sibling: Option<Index>,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -29,26 +32,60 @@ impl<T> Tree<T> {
 
     pub fn add_root(&mut self, value: T) -> Index {
         assert!(self.is_empty());
-        Index(self.nodes.insert(Node::root(value)))
+        Index(self.nodes.insert(Node {
+            value,
+            parent: None,
+            first_child: None,
+            last_child: None,
+            next_sibling: None,
+            prev_sibling: None,
+        }))
     }
 
     pub fn add_leaf(&mut self, parent: Index, value: T) -> Index {
-        assert!(self.get(parent).is_some());
-        let idx = Index(self.nodes.insert(Node::leaf(value, parent)));
-        self.get_mut(parent).unwrap().children.push(idx);
+        let parent_node = &self[parent];
+        let prev_sibling = parent_node.last_child;
+
+        let idx = Index(self.nodes.insert(Node {
+            value,
+            parent: Some(parent),
+            first_child: None,
+            last_child: None,
+            next_sibling: None,
+            prev_sibling,
+        }));
+
+        let parent_node = &mut self[parent];
+        parent_node.first_child.get_or_insert(idx);
+        parent_node.last_child = Some(idx);
+        if let Some(prev_sibling) = prev_sibling {
+            self[prev_sibling].next_sibling = Some(idx);
+        }
         idx
     }
 
     pub fn remove_leaf(&mut self, idx: Index) -> Option<T> {
         assert!(
-            self.get(idx).map_or(true, |n| n.children.is_empty()),
+            self.get(idx).map_or(true, |n| n.first_child.is_none()),
             "Node is not a leaf"
         );
         let node = self.nodes.try_remove(idx.as_usize())?;
         if let Some(parent) = node.parent {
-            let parent = self.get_mut(parent).unwrap();
-            let pos = parent.children.iter().position(|i| *i == idx).unwrap();
-            parent.children.remove(pos);
+            let parent = &mut self[parent];
+
+            if parent.first_child.is_some_and(|i| i == idx) {
+                parent.first_child = node.next_sibling;
+            }
+            if parent.last_child.is_some_and(|i| i == idx) {
+                parent.last_child = node.prev_sibling;
+            }
+
+            if let Some(prev_sibling) = node.prev_sibling {
+                self[prev_sibling].next_sibling = node.next_sibling;
+            }
+            if let Some(next_sibling) = node.next_sibling {
+                self[next_sibling].prev_sibling = node.prev_sibling;
+            }
         }
         Some(node.value)
     }
@@ -66,23 +103,21 @@ impl<T> Tree<T> {
     }
 }
 
+impl<T> std::ops::Index<Index> for Tree<T> {
+    type Output = Node<T>;
+
+    fn index(&self, index: Index) -> &Self::Output {
+        self.get(index).unwrap()
+    }
+}
+
+impl<T> std::ops::IndexMut<Index> for Tree<T> {
+    fn index_mut(&mut self, index: Index) -> &mut Self::Output {
+        self.get_mut(index).unwrap()
+    }
+}
+
 impl<T> Node<T> {
-    fn root(value: T) -> Self {
-        Self {
-            value,
-            parent: None,
-            children: Vec::new(),
-        }
-    }
-
-    fn leaf(value: T, parent: Index) -> Self {
-        Self {
-            value,
-            parent: Some(parent),
-            children: Vec::new(),
-        }
-    }
-
     pub fn get(&self) -> &T {
         &self.value
     }
