bc61afb79bdb63256579a3daa60db824cc3ac930
Vixu <lonevixu@gmail.com>
2024-07-14 13:13:30 +0200

    use slab for Tree's storage

--- unlox-tree/src/lib.rs
+++ unlox-tree/src/lib.rs
@@ -1,6 +1,8 @@
+use slab::Slab;
+
 /// Generic tree data structure implementation.
 pub struct Tree<T> {
-    nodes: Vec<Option<Node<T>>>,
+    nodes: Slab<Node<T>>,
 }
 
 pub struct Node<T> {
@@ -27,26 +29,14 @@ impl<T> Tree<T> {
 
     pub fn add_root(&mut self, value: T) -> Index {
         assert!(self.is_empty());
-        self.add(Node::root(value))
+        Index(self.nodes.insert(Node::root(value)))
     }
 
     pub fn add_leaf(&mut self, parent: Index, value: T) -> Index {
         assert!(self.get(parent).is_some());
-        let idx = self.add(Node::leaf(value, parent));
+        let idx = Index(self.nodes.insert(Node::leaf(value, parent)));
         self.get_mut(parent).unwrap().children.push(idx);
         idx
-    }
-
-    fn add(&mut self, node: Node<T>) -> Index {
-        let idx = self.nodes.iter_mut().position(|slot| slot.is_none());
-        if let Some(idx) = idx {
-            self.nodes[idx] = Some(node);
-            Index(idx)
-        } else {
-            let idx = self.nodes.len();
-            self.nodes.push(Some(node));
-            Index(idx)
-        }
     }
 
     pub fn remove_leaf(&mut self, idx: Index) -> Option<T> {
@@ -54,7 +44,7 @@ impl<T> Tree<T> {
             self.get(idx).map_or(true, |n| n.children.is_empty()),
             "Node is not a leaf"
         );
-        let node = self.nodes.get_mut(idx.0)?.take()?;
+        let node = self.nodes.try_remove(idx.as_usize())?;
         if let Some(parent) = node.parent {
             let parent = self.get_mut(parent).unwrap();
             let pos = parent.children.iter().position(|i| *i == idx).unwrap();
@@ -64,11 +54,11 @@ impl<T> Tree<T> {
     }
 
     pub fn get(&self, idx: Index) -> Option<&Node<T>> {
-        self.nodes.get(idx.0)?.as_ref()
+        self.nodes.get(idx.0)
     }
 
     pub fn get_mut(&mut self, idx: Index) -> Option<&mut Node<T>> {
-        self.nodes.get_mut(idx.0)?.as_mut()
+        self.nodes.get_mut(idx.0)
     }
 
     pub fn is_empty(&self) -> bool {
